76a633f79a99d85f92dfcedeaea6b3857c6a41c9
Eric Versteeg <erversteeg@gmail.com>
2024-11-09 15:44:47 -0500

    Fix empty bar border and background height issue

--- src/main/java/com/ericversteeg/frosthprun/view/RSBar.java
+++ src/main/java/com/ericversteeg/frosthprun/view/RSBar.java
@@ -101,7 +101,13 @@ public class RSBar extends RSView {
         };
 
         graphics.setPaint(colorWithOpacity(new Color(0, 0, 0, 156)));
-        graphics.fillRect(origin.x, origin.y, w, h);
+        int backgroundHeight = h;
+        if (index == groupSize - 1)
+        {
+            backgroundHeight += 1;
+        }
+
+        graphics.fillRect(origin.x, origin.y, w, backgroundHeight);
         //graphics.fillRect(origin.x + x - 1, origin.y - 1, w + 2, h + 2);
 
         if (barStyle == BarStyle.ROUND)
@@ -135,33 +141,36 @@ public class RSBar extends RSView {
         graphics.setPaint(gradientPaint);
         graphics.fillRect(origin.x + x, origin.y + y, barSize, h);
 
-        graphics.setColor(outerBorderColor);
-        if (index == 0)
+        if (barSize > 0)
         {
-            graphics.drawLine(origin.x + x - 2, origin.y + y - 2, origin.x + x + barSize + 1, origin.y + y - 2);
-            graphics.drawLine(origin.x + x - 2, origin.y + y - 1, origin.x + x + barSize + 1, origin.y + y - 1);
-        }
-        if (index == groupSize - 1)
-        {
-            graphics.drawLine(origin.x + x - 2, origin.y + y + h, origin.x + x + barSize + 1, origin.y + y + h);
-            graphics.drawLine(origin.x + x - 2, origin.y + y + h + 1, origin.x + x + barSize + 1, origin.y + y + h + 1);
-            graphics.drawLine(origin.x + x - 2, origin.y + y + h + 2, origin.x + x + barSize + 1, origin.y + y + h + 2);
-        }
+            graphics.setColor(outerBorderColor);
+            if (index == 0)
+            {
+                graphics.drawLine(origin.x + x - 2, origin.y + y - 2, origin.x + x + barSize + 1, origin.y + y - 2);
+                graphics.drawLine(origin.x + x - 2, origin.y + y - 1, origin.x + x + barSize + 1, origin.y + y - 1);
+            }
+            if (index == groupSize - 1)
+            {
+                graphics.drawLine(origin.x + x - 2, origin.y + y + h, origin.x + x + barSize + 1, origin.y + y + h);
+                graphics.drawLine(origin.x + x - 2, origin.y + y + h + 1, origin.x + x + barSize + 1, origin.y + y + h + 1);
+                graphics.drawLine(origin.x + x - 2, origin.y + y + h + 2, origin.x + x + barSize + 1, origin.y + y + h + 2);
+            }
 
-        graphics.drawLine(origin.x + x - 2, origin.y + y, origin.x + x - 2, origin.y + y + h - 1);
-        graphics.drawLine(origin.x + x - 1, origin.y + y, origin.x + x - 1, origin.y + y + h - 1);
+            graphics.drawLine(origin.x + x - 2, origin.y + y, origin.x + x - 2, origin.y + y + h - 1);
+            graphics.drawLine(origin.x + x - 1, origin.y + y, origin.x + x - 1, origin.y + y + h - 1);
 
-        graphics.drawLine(origin.x + x + barSize, origin.y + y, origin.x + x + barSize, origin.y + y + h - 1);
-        graphics.drawLine(origin.x + x + barSize + 1, origin.y + y, origin.x + x + barSize + 1, origin.y + y + h - 1);
+            graphics.drawLine(origin.x + x + barSize, origin.y + y, origin.x + x + barSize, origin.y + y + h - 1);
+            graphics.drawLine(origin.x + x + barSize + 1, origin.y + y, origin.x + x + barSize + 1, origin.y + y + h - 1);
 
-        graphics.setColor(innerBorderColor);
-        graphics.drawLine(origin.x + x, origin.y + y, origin.x + x + barSize - 1, origin.y + y);
-        if (index == groupSize - 1)
-        {
-            graphics.drawLine(origin.x + x, origin.y + y + h, origin.x + x + barSize - 1, origin.y + y + h);
+            graphics.setColor(innerBorderColor);
+            graphics.drawLine(origin.x + x, origin.y + y, origin.x + x + barSize - 1, origin.y + y);
+            if (index == groupSize - 1)
+            {
+                graphics.drawLine(origin.x + x, origin.y + y + h, origin.x + x + barSize - 1, origin.y + y + h);
+            }
+            graphics.drawLine(origin.x + x, origin.y + y + 1, origin.x + x, origin.y + y + h - 1);
+            graphics.drawLine(origin.x + x + barSize - 1, origin.y + y + 1, origin.x + x + barSize - 1, origin.y + y + h - 1);
         }
-        graphics.drawLine(origin.x + x, origin.y + y + 1, origin.x + x, origin.y + y + h - 1);
-        graphics.drawLine(origin.x + x + barSize - 1, origin.y + y + 1, origin.x + x + barSize - 1, origin.y + y + h - 1);
     }
 
     private Color hueColor(Color color, float hue)
